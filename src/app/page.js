477097ba15ec668b7e3732496029c2a43c9fc469
sarathsda <sarathsda@gmail.com>
2023-11-29 10:27:33 +0530

    put api key

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Results from "@/components/Results"
 
 
-const API_KEY = process.env.API_KEY
+// const API_KEY = process.env.API_KEY
+ const API_KEY = f0a67b258aea1c9ea1b2aebe90af5bb8
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?"movie/top_rated":"trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,
