8e3eb04eb5074cd3b310c48c1134cd1fc21e426b
sarathsda <sarathsda@gmail.com>
2023-12-07 13:05:18 +0530

    error check

--- src/components/Header.js
+++ src/components/Header.js
@@ -60,11 +60,11 @@ const Header = () => {
 
   const getApiEndpoint = (selectedOption) => {
     if (selectedOption === "popularity") {
-      return "search/trending";
+      return "search/trending/all/week";
     } else if (selectedOption === "top_rated") {
       return "movie/top_rated";
     } else if (selectedOption === "release_date") {
-      return "search/release-date";
+      return "discover/movie";
     }
   };
 
