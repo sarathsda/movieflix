e2c10542890cc6509a1aed70416eb6ee5ffb9c1c
sarathsda <sarathsda@gmail.com>
2023-12-07 14:44:45 +0530

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,134 @@
+// "use client"
+// import React, { useState, useEffect } from "react";
+// import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
+// import Link from "next/link";
+// import MenuItem from "./MenuItem";
+// import DarkMode from "./DarkMode";
+// import Image from 'next/image'
+
+
+// const Header = () => {
+//   const [searchQuery, setSearchQuery] = useState("");
+//   const [sortingOption, setSortingOption] = useState("popularity");
+//   const [searchResults, setSearchResults] = useState([]);
+
+//   useEffect(() => {
+//     if (searchQuery) {
+//       // Fetch movie search results from TMDb API
+//       const fetchSearchResults = async () => {
+//         try {
+//           const apiKey = "f0a67b258aea1c9ea1b2aebe90af5bb8";
+//           const response = await fetch(
+//             `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`
+//           );
+//           const data = await response.json();
+//           setSearchResults(data.results);
+//         } catch (error) {
+//           console.error("Error fetching search results:", error);
+//         }
+//       };
+
+//       fetchSearchResults();
+//     } else {
+//       setSearchResults([]);
+//     }
+//   }, [searchQuery]);
+
+//   const handleSearch = () => {
+//     // The search results are already fetched in the useEffect
+//     console.log("Searching for:", searchQuery);
+//   };
+
+//   const handleSortChange = (e) => {
+//     setSortingOption(e.target.value);
+//     // Implement your sorting logic here
+//     console.log("Sorting by:", e.target.value);
+//   };
+
+//   return (
+//     <>
+//       <div className="flex justify-between mx-2 max-w-5xl sm:mx-auto items-center py-6">
+//         <div className="flex">
+//           <MenuItem title={"HOME"} address={"/"} Icon={AiFillHome} />
+//           <MenuItem title={"About"} address={"/about"} Icon={AiFillInfoCircle} />
+//         </div>
+//         <div className="flex items-center space-x-5">
+//           <DarkMode />
+
+//           {/* Search input */}
+//           <div className="relative">
+//             <input
+//               type="text"
+//               placeholder="Search..."
+//               value={searchQuery}
+//               onChange={(e) => setSearchQuery(e.target.value)}
+//               className="border rounded-md p-2"
+//             />
+//             <button
+//               onClick={handleSearch}
+//               className="absolute top-0 right-0 mt-2 mr-2 cursor-pointer"
+//             >
+//               {/* Add an icon or text here */}
+//               Search
+//             </button>
+//           </div>
+
+//           {/* Sorting options */}
+//           <select
+//             value={sortingOption}
+//             onChange={handleSortChange}
+//             className="border rounded-md p-2 ml-2"
+//           >
+//             <option value="popularity">Popularity</option>
+//             <option value="release_date">Release Date</option>
+//           </select>
+
+//           <Link href="/">
+//             <h2 className="text-2xl">
+//               <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg">
+//                 Movie
+//               </span>
+//               <span className="text-xl hidden sm:inline">Flix</span>
+//             </h2>
+//           </Link>
+//         </div>
+//       </div>
+
+//     {/* Display search results as a list of movie posters */}
+//     <div className="flex flex-wrap justify-center ">
+//         {searchResults.map((result) => (
+//           <Link key={result.id} href={`/movie/${result.id}`}>
+//             <div className="cursor-pointer m-2 group relative">
+//               <Image
+//                 src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+//                 width={200}
+//                 height={300}
+//                 alt={result.title || result.name}
+//                 className="rounded-md transition duration-200 transform group-hover:scale-105"
+//               />
+//               <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 bg-black bg-opacity-50 rounded-md">
+//                 <div className="text-white text-center">
+//                   <p className="text-lg font-bold">{result.title || result.name}</p>
+//                   <p>{result.release_date || result.first_air_date}</p>
+//                 </div>
+//               </div>
+//             </div>
+//           </Link>
+//         ))}
+//       </div>
+//     </>
+    
+//   );
+// };
+// export default Header;
+
 "use client"
 import React, { useState, useEffect } from "react";
 import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
 import Link from "next/link";
 import MenuItem from "./MenuItem";
 import DarkMode from "./DarkMode";
-import Image from 'next/image'
-
+import Image from "next/image";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -47,28 +170,27 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex justify-between mx-2 max-w-5xl sm:mx-auto items-center py-6">
-        <div className="flex">
+      <div className="flex flex-col sm:flex-row justify-between mx-2 max-w-5xl sm:mx-auto items-center py-6">
+        <div className="flex mb-2 sm:mb-0">
           <MenuItem title={"HOME"} address={"/"} Icon={AiFillHome} />
           <MenuItem title={"About"} address={"/about"} Icon={AiFillInfoCircle} />
         </div>
-        <div className="flex items-center space-x-5">
+        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-5">
           <DarkMode />
 
           {/* Search input */}
-          <div className="relative">
+          <div className="relative flex-shrink-0 w-full sm:w-auto">
             <input
               type="text"
               placeholder="Search..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="border rounded-md p-2"
+              className="border rounded-md p-2 w-full"
             />
             <button
               onClick={handleSearch}
               className="absolute top-0 right-0 mt-2 mr-2 cursor-pointer"
             >
-              {/* Add an icon or text here */}
               Search
             </button>
           </div>
@@ -77,7 +199,7 @@ const Header = () => {
           <select
             value={sortingOption}
             onChange={handleSortChange}
-            className="border rounded-md p-2 ml-2"
+            className="border rounded-md p-2 mt-2 sm:mt-0 sm:ml-2"
           >
             <option value="popularity">Popularity</option>
             <option value="release_date">Release Date</option>
@@ -94,8 +216,8 @@ const Header = () => {
         </div>
       </div>
 
-    {/* Display search results as a list of movie posters */}
-    <div className="flex flex-wrap justify-center ">
+      {/* Display search results as a list of movie posters */}
+      <div className="flex flex-wrap justify-center">
         {searchResults.map((result) => (
           <Link key={result.id} href={`/movie/${result.id}`}>
             <div className="cursor-pointer m-2 group relative">
@@ -117,7 +239,7 @@ const Header = () => {
         ))}
       </div>
     </>
-    
   );
 };
+
 export default Header;
